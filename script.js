fcf1893d3a549dba88a2242ca5cac17a6cdbb8a6
j-goodman <johncurtisgoodman@gmail.com>
2026-04-23 20:09:19 -0400

    Add basic data formatting.

--- script.js
+++ script.js
@@ -2,4 +2,20 @@ fetch(`http://localhost:3000/movies`)
 .then(response => response.json())
 .then(data => {
     console.log(data)
+    loadDataToPage(data)
 })
+
+const movieListContainer = document.getElementById("movie-list-container")
+
+const loadDataToPage = data => {
+    data.forEach(movie => {
+        const liElement = document.createElement("li")
+        const pElement = document.createElement("p")
+        const divElement = document.createElement("div")
+        pElement.innerText = `${movie.title}, ${movie.year}, starring ${movie.starring[0]} and ${movie.starring[1]}.`
+        divElement.innerText = movie.watched ? `WATCHED` : `NOT WATCHED YET`
+        liElement.appendChild(pElement)
+        liElement.appendChild(divElement)
+        movieListContainer.appendChild(liElement)
+    })
+}
